8b32fc97fc6a019234eda36d4bface6779aa33c9
black-dragon74 <niryadav@redhat.com>
2024-02-19 21:56:57 +0530

    Remove test code
    Signed-off-by: black-dragon74 <niryadav@redhat.com>

--- main.go
+++ main.go
@@ -3,12 +3,7 @@ package main
 import (
 	"github.com/black-dragon74/rht-trn/app"
 	"github.com/black-dragon74/rht-trn/initialize"
-	"github.com/black-dragon74/rht-trn/types"
 )
-
-type StudentWrapper struct {
-	Students []types.Student `yaml:"students"`
-}
 
 func main() {
 	lgr := initialize.Logger()
